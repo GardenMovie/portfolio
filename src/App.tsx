55171483a7706cbf1d97d3fa8f69fe35a7f8b652
GardenMovie <dody.gallerani@aluno.ufabc.edu.br>
2026-01-10 18:02:26 -0300

    Fix merge conflicts on app.tsx and projects.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-
 import Navbar from "@/components/layout/Navbar"
 import Hero from "@/sections/Hero"
 import { Education } from "./sections/Education"
 import { Experience } from "./sections/Experience"
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Projects } from "./sections/Projects"
 import { Language } from "./sections/Language"
-=======
-import { Languages } from "./sections/Languages"
-import { Projects } from "./sections/Projects"
->>>>>>> 76b8a19 (Feat: add project cards with details and stack to new section for projects)
-=======
-import { Projects } from "./sections/Projects"
-import { Language } from "./sections/Language"
->>>>>>> 899c0f3 (Feat: update project sections; refactor Navbar and styling)
 
 export default function App() {
   return (
@@ -23,20 +12,9 @@ export default function App() {
       <main className="flex flex-col mx-auto px-10 gap-10 md:max-w-6xl py-20 pt-24">
         <Hero />
         <Experience />
-<<<<<<< HEAD
-        <Projects />
-        <Education/>
-<<<<<<< HEAD
-        <Language />
-=======
-        <Languages />
-        <Projects />
->>>>>>> 76b8a19 (Feat: add project cards with details and stack to new section for projects)
-=======
         <Projects />
         <Education/>
         <Language />
->>>>>>> 899c0f3 (Feat: update project sections; refactor Navbar and styling)
       </main>
     </section>
   )
