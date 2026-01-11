dc6c0f4b3be294e0e01c1c822e0b7195fcb864c6
GardenMovie <dody.gallerani@aluno.ufabc.edu.br>
2026-01-11 16:14:16 -0300

    Fix: change flexbox direction of the about section when using smaller screens

--- src/sections/AboutSimple.tsx
+++ src/sections/AboutSimple.tsx
@@ -5,12 +5,12 @@ import IconLinks from "../components/common/IconLinks";
 const AboutSimple: React.FC = () => {
   return (
     <section className="relative w-full mb-2">
-      <Card className="flex flex-row p-8">
+      <Card className="flex md:flex-row p-8">
         <div className="flex flex-col text-3xl font-bold whitespace-pre leading-tight text-left min-w-[120px]">
           <span>Dody</span>
           <span>Gallerani.</span>
         </div>
-        <div className="flex flex-col flex-1 items-end gap-2">
+        <div className="flex flex-col flex-1 md:items-end gap-2">
           <span className="text-lg font-medium">Let&apos;s connect</span>
           <IconLinks row={true} />
         </div>
