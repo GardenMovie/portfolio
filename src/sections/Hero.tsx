f3ea40ef970ed775adc7d91a593b6ae728f69379
GardenMovie <dody.gallerani@aluno.ufabc.edu.br>
2026-01-11 14:01:30 -0300

    Feat: pfp added back on hero section, rounded

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,8 +5,13 @@ export default function Hero() {
   return (
     <section
       id="home"
-      className="min-h-[30vh] md:min-h-screen flex items-center justify-center flex-col-reverse md:flex-row gap-3 scroll-mt-20"
+      className="min-h-[30vh] md:min-h-screen flex items-center justify-center md:flex-row flex-col gap-9 scroll-mt-20"
     >
+      <img
+        className="w-1/3 md:w-1/6 rounded-full md:mt-0 mt-20 ring-4 ring-primary"
+        src="pfp.jpg"
+        alt="Profile Picture"
+      ></img>
       <div className="text-left space-y-2">
         <h1 className="text-4xl sm:text-5xl font-semibold tracking-tight">
           Hey! I'm <span className="text-primary">Dody</span> (Dod-gee)
@@ -20,11 +25,6 @@ export default function Hero() {
         </p>
         <IconLinks />
       </div>
-      {/* <img
-        className="w-1/2 md:w-1/3 rounded-4xl md:rounded-lg "
-        src="pfp.jpg"
-        alt="Profile Picture"
-      ></img> */}
     </section>
   );
 }
