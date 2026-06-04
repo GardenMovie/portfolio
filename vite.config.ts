b2e6e1d4fb32adf9c0746c6c46b5cb35e171df70
GardenMovie <dodygardenmovie@gmail.com>
2026-06-04 16:11:24 -0300

    changed plugin to optimize images, seems to be working

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,18 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import imagemin from "vite-plugin-imagemin"
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    imagemin({
-      gifsicle: { optimizationLevel: 7 },
-      optipng: { optimizationLevel: 7 },
-      mozjpeg: { quality: 80 },
-      webp: { quality: 80 },
-    }),
+    ViteImageOptimizer({
+      png: {
+    // https://sharp.pixelplumbing.com/api-output#png
+    quality: 70,
+  } })
   ],
   resolve: {
     alias: {
